c6320e3e9f8b0273c7c89b0b361da45d9bd9a69c
ByJhona <jhonatantecno23@gmail.com>
2020-11-18 14:43:19 -0300

    Porjeto incluido

---
+++ cyberTop/src/classes/Alunos.java
@@ -0,0 +1,26 @@
+package classes;
+
+// Subclasse(classe filha) de Dados
+public class Alunos extends Dados{
+    // Declaração de atributos
+    //private Turmas turma;
+    private String endereco;
+    
+    
+    /*
+    public Turmas getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turmas turma) {
+        this.turma = turma;
+    }
+    */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
