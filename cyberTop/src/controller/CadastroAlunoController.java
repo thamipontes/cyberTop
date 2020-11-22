0c67ede73c15a517432bde6d159e73c7c385e6aa
Thamires <thamiresdepontesdasilva@gmail.com>
2020-11-22 12:11:31 -0300

    Arruma documentação e regra de negocio

--- cyberTop/src/controller/CadastroAlunoController.java
+++ cyberTop/src/controller/CadastroAlunoController.java
@@ -20,9 +20,11 @@ import telas.CadastroTurma;
  *
  * @author thamires
  */
+
+/*Classe que controla todas as regras de negócios do cadastro do Aluno*/
 public class CadastroAlunoController {
     
-    //Atributo que possui a tela CadastroAluno
+    //Declaração do atributo que possui a tela CadastroAluno
     private CadastroAluno view;
 
     //Construtor
@@ -30,28 +32,29 @@ public class CadastroAlunoController {
         this.view = view;
     }
 
-    
-    //Método que salva o aluno no banco de dados
+    /*
+        Método: salvarAluno
+        Parâmetros: vazio
+        Descrição: pega os dados inseridos na tela e salva no banco de dados    
+    */
     public void salvarAluno() throws ParseException{
         
         /*Pegas as informações passadas nos campos da tela*/
         String nome = view.getTxtNome().getText();
         String cpf = view.getTxtCPF().getText();
+        String telefone = view.getTxtTelefone().getText();
+        String endereco = view.getTxtLogradouro().getText();
+        String cep = view.getTxtCEP().getText();
         
         /*Transformando a data string para Date*/        
         String dataNascimentoString = view.getTxtDataNascimento().getText();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy"); //define padrão que a data deve obedecer
         Calendar dataNascimento = Calendar.getInstance();
-        dataNascimento.setTime(sdf.parse(dataNascimentoString));
-        
-        String telefone = view.getTxtTelefone().getText();
-        String endereco = view.getTxtLogradouro().getText();
-        
+        dataNascimento.setTime(sdf.parse(dataNascimentoString));  
+    
        /*Tratando o dado Genero*/       
         char genero = formatarGenero((view.getCmbGenero().getSelectedIndex()));        
-       
-        String cep = view.getTxtCEP().getText();
-        
+    
        /*Tratando o dado Cor Raça*/
         String corRaca = view.getCmbCorRaca().getSelectedItem().toString(); 
        
@@ -76,10 +79,10 @@ public class CadastroAlunoController {
     }
     
     /*
-    Método: formatarGenero
-    Parâmetros: index do tipo inteiro
-    Descrição: Confere qual index foi selecionado na tela cadastro aluno do campo gênero 
-    e retorna um char que indica se o gênero é não informado, masculino, feminino e outros.
+        Método: formatarGenero
+        Parâmetros: index do tipo inteiro
+        Descrição: Confere qual index foi selecionado na tela cadastro aluno do campo gênero 
+    e retorna um char que indica se o gênero é não informado, masculino, feminino e outros    
     */
     public char formatarGenero(int index){        
         if(index == 1) return 'I'; //Prefiro não informar
@@ -87,6 +90,41 @@ public class CadastroAlunoController {
         if(index == 3) return 'F'; //Feminino
         if(index == 4) return 'O'; //Outros       
         return ' '; 
+    }
+    
+    
+    public void exibirAlertarCampos(){        
+        //Descobre se alguns dos campos ficaram vazios
+        if(view.getTxtNome().getText().equals("")                       ||
+            view.getTxtCPF().getText().equals("   .   .   -  ")         || 
+            view.getTxtDataNascimento().getText().equals("  /  /    ")  ||
+            view.getCmbGenero().getSelectedItem().equals("Selecione")   ||
+            view.getCmbCorRaca().getSelectedItem().equals("Selecione")  ||
+            view.getTxtTelefone().getText().equals("(  )      -    ")   ||
+            view.getTxtCEP().getText().equals("     -   ")              ||
+            view.getTxtLogradouro().getText().equals("")
+                ){            
+            
+            //Mostra a mensagem para preencher todos os campos
+            JOptionPane.showMessageDialog(null, "Preencha todos os campos!");            
+        }
+    }
+    
+    
+    public void limparCampos(){    
+    //Insere o cursor no txtNome e o deixa em foco
+        view.getTxtNome().requestFocus();
+
+        // Limpa todos os campos
+        view.getTxtNome().setText("");
+        view.getTxtCPF().setText("");
+        view.getTxtDataNascimento().setText("");
+        view.getCmbCorRaca().setSelectedItem("Selecione");
+        view.getCmbGenero().setSelectedItem("Selecione");
+        view.getTxtTelefone().setText("");
+        view.getTxtCEP().setText("");
+        view.getTxtLogradouro().setText("");
+    
     }
     
     
