0c34c31292b8adbd53c461352a1786b648368685
ByJhona <jhonatantecno23@gmail.com>
2020-11-22 11:56:22 -0300

    Tela cadastro turma

--- cyberTop/src/telas/Login.java
+++ cyberTop/src/telas/Login.java
@@ -248,7 +248,7 @@ public class Login extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        // Metodo teste
        //this.setVisible(false);
-       new CadastroAluno().setVisible(true);
+       new Jhonatan().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
