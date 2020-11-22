d270bc8addb1dc0b080f686f5fad7bb79f14374f
Thamires <thamiresdepontesdasilva@gmail.com>
2020-11-22 19:00:48 -0300

    Documenta melhor CadastroAlunoController

--- cyberTop/src/controller/CadastroAlunoController.java
+++ cyberTop/src/controller/CadastroAlunoController.java
@@ -51,19 +51,26 @@ public class CadastroAlunoController {
         String endereco = view.getTxtLogradouro().getText();
         String cep = view.getTxtCEP().getText();
         
-        /*Transformando a data string para Date*/        
+        /*Tratando o dado Data de Nascimento*/   
+        //Pega o texto que está no campo data de nascimento
         String dataNascimentoString = view.getTxtDataNascimento().getText();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy"); //define padrão que a data deve obedecer
+        //define padrão que a data deve obedecer
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy"); 
+        //Transforma o atributo em tipo calendar
         Calendar dataNascimento = Calendar.getInstance();
+        //Seta a data no atributo data nascimento
         dataNascimento.setTime(sdf.parse(dataNascimentoString));  
     
-       /*Tratando o dado Genero*/       
+       /*Tratando o dado Genero*/  
+       //Variável que recebe o return do método formatarGenero que está implementado no final do código
         char genero = formatarGenero((view.getCmbGenero().getSelectedIndex()));        
     
        /*Tratando o dado Cor Raça*/
+       //Transforma o o texto do item selecionado em string e guarda na variável
         String corRaca = view.getCmbCorRaca().getSelectedItem().toString();
         
-       /*Tratando a turma*/
+       /*Tratando o dado da turma*/
+       //Transforma String em inteiro
         int turmaId = Integer.parseInt(view.getTxtTurma().getText());
     
        /*Instaciando o objeto aluno com os dados recebidos pela tela*/
@@ -72,16 +79,19 @@ public class CadastroAlunoController {
        /*Conexão com o banco de dados*/
         Connection conexao;        
         try {
-            
+            //Faz a conexão com o banco
             conexao = new Conexao().getConnection();
+            //Passa a conexão para a classe AlunosDAO que possui o CRUD
             AlunosDAO alunoDAO = new AlunosDAO(conexao);
-            alunoDAO.insert(aluno);
-        
+            //Chama o método de inserção            
+            alunoDAO.insert(aluno);        
             //Mensagem de aluno cadastrado com sucesso
             JOptionPane.showMessageDialog(null, "Aluno cadastrado com sucesso");            
             
         } catch (SQLException ex) {
             Logger.getLogger(CadastroTurma.class.getName()).log(Level.SEVERE, null, ex);
+            //Caso dê erro mostra essa tela
+            JOptionPane.showMessageDialog(null, "Falha ao cadastrar dado no banco");
         }  
     
     }
@@ -100,7 +110,12 @@ public class CadastroAlunoController {
         return ' '; 
     }
     
-    
+    /*
+        Método: exibirAlertarCampos
+        Parâmetros: vazio
+        Descrição: confere se teve algum campo que ficou sem ser preenchido e mostra na tela
+        uma mensagem para preencher todos os campos.
+    */
     public boolean exibirAlertarCampos(){        
         //Descobre se alguns dos campos ficaram vazios
         if(view.getTxtNome().getText().equals("")                       ||
@@ -122,7 +137,11 @@ public class CadastroAlunoController {
         return false;
     }
     
-    
+    /*
+        Método: limparCampos
+        Parâmetros: vazio
+        Descrição: deixa todos os campos da tela vazio
+    */
     public void limparCampos(){    
     //Insere o cursor no txtNome e o deixa em foco
         view.getTxtNome().requestFocus();
@@ -139,43 +158,66 @@ public class CadastroAlunoController {
     
     }
     
-    
+    /*
+        Método: carregarDadosTurma
+        Parâmetros: vazio
+        Descrição: retorna a lista de turmas que está persistido no banco de dados
+    */
      private ArrayList<Turmas> carregarDadosTurma() throws SQLException {
+        //Faz a conexão com o banco 
         Connection conexao;
         conexao = new Conexao().getConnection();
+        //Passa a conexão para a classe TurmaDAO para realizar o CRUD
         TurmaDAO turmaDAO = new TurmaDAO(conexao);
+        //Chama o método findAll que retorna uma lista de turmas que está no banco de dados
         ArrayList<Turmas> turmasBanco = turmaDAO.findAll();
         return turmasBanco;
     }
     
-    
+    /*
+        Método: inserirDadosTurmaTabela
+        Parâmetros: vazio
+        Descrição: insere as informações da tabela turma do banco de dados na tabela da tela
+    */
     public void inserirDadosTurmaTabela() throws SQLException{
         
+        //Variável que possui uma lista de turmas vinda do banco de dados
         ArrayList<Turmas> turmasBanco = carregarDadosTurma();
-    
+        
+        //Cria o modelo da tabela
         DefaultTableModel modelo = new DefaultTableModel(new Object[] {"Id", "Nome", "Horário", "Período", "Vagas"}, 0);
         
+        //Percorre a lista de turmas vinda do banco e insere em cada linha
         turmasBanco.forEach(e -> {        
             Object linha[] = new Object[]{e.getId(), e.getNome(), e.getHorario(), e.getPeriodo(), e.getVagas()};
             modelo.addRow(linha);        
         });        
         
+        //Seta na tabela todos os valores vindo da lista
         view.getTblTurmas().setModel(modelo);
     
     }
-
-   
+    
+    /*
+        Método: salvarLinhaTurma
+        Parâmetros: vazio
+        Descrição: insere no campo turma da tela o id da turma que foi selecionada da tabela 
+        ao clicar na linha
+    */   
     public void salvarLinhaTurma() throws SQLException {
-        
+        //Variável que possui uma lista de turmas vinda do banco de dados
         ArrayList<Turmas> turmasBanco = carregarDadosTurma();
         
+        //Variável que possui a linha que foi clicada
         int linha = view.getTblTurmas().getSelectedRow();
         
         //Conferindo se é uma linha inválida
         if(linha >= 0 && linha<turmasBanco.size()){
-        
+            //variável que recebe a turma que está na linha selecionada
             Turmas turma = turmasBanco.get(linha);
+            //Seta no campo turma da tela o id da turma
             view.getTxtTurma().setText(String.valueOf(turma.getId()));
+            //Desabilita o campo turma da tela
             view.getTxtTurma().setEnabled(false);
 
         }  
